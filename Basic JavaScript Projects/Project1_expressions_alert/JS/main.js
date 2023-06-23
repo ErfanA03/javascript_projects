542db7005b362cc365a5a29c4a02d9bbaf14b539
Erfan Arsin <erfanbro46@gmail.com>
2023-06-23 17:23:10 -0600

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var a = "Hello", b = ",", c = " World", d = "!"; //Assigned multiple variables
+window.alert("Welcome to my amazing website!") // Alerted the user via a display box that shows
+document.write(a + c); //using document.write method to print variable to screen.
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
+document.write(Sent1 + Sent2);
+document.write(3*3);
