1d84eabad9b8056f666047d786fc78ee4b2946e6
Erfan Arsin <erfanbro46@gmail.com>
2023-06-26 17:25:39 -0600

    PROJECT6_TERNARY_OPERATORS_CONSTRUCTORS SUBMISSION ASSIGNMENT

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,7 +1,37 @@
-function canVote() {
-  var age, canVote;
+function rideFunc() {
+  var age, can_Vote;
   age = document.getElementById("age").value;
-  canVote =
-    age < 18 ? "You are too young to vote" : "You are old enough to vote";
-  document.getElementById("canVote").innerHTML = canVote;
+  can_Vote =
+    age < 18 ? "You are not old enough to vote" : "You are old enough to vote"; //Function that checkes whether or not one is over the age of 18 and displays text saying whethere the user is or not.//
+  document.getElementById("age_text").innerHTML = can_Vote;
+}
+
+function Vehicle(Make, Model, Year, Color) {
+  this.Vehicle_Make = Make;
+  this.Vehicle_Model = Model; //Function constructor
+  this.Vehicle_Year = Year;
+  this.Vehicle_Color = Color;
+}
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black"); //Creating new instances of the Vehicle class and giving them arguments for the parameters.
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+  document.getElementById("Keywords_and_Constructors").innerHTML = //Using a function to display to the element with the id of "Keywords_and_Constructors" when clicked upon.
+    "Erik drives a " +
+    Erik.Vehicle_Color +
+    "-colored " +
+    Erik.Vehicle_Model +
+    " manufactured in " +
+    Erik.Vehicle_Year;
+}
+
+function nestedFunc() {
+  document.getElementById("Nested_Function").innerHTML = greeting(); //Demonstrating the use of nested function -- Functions that are inside other functions. Here I am defining a "nestedFunc()" function and calling the "greeting()"" function with the "return" keyword which then returns the value of invoking the "nestedGreeting()" function which is "HELLO, WORLD!" to the outer function which is greeting() and then "greeting()" itself gets return to where it was called, from there the value is stored as the innerHTML for the element.)
+  function greeting() {
+    function nestedGreeting() {
+      return "HELLO, WORLD!";
+    }
+    return nestedGreeting();
+  }
 }
